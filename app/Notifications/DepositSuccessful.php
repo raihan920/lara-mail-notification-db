be9bebe5d228d2ba2ae6e76d46d5b3653216114c
Raihan Chowdhury <raihan92@rocketmail.com>
2023-09-08 21:56:57 +0600

    Done as it is.

---
+++ app/Notifications/DepositSuccessful.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DepositSuccessful extends Notification
+{
+    use Queueable;
+    protected $amount;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail','database'];    //to store in database
+        // return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->greeting('Hello,')
+                    ->line('your deposit of '.$this->amount.' was successful')
+                    ->action('View Dashboard', url('/home'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' => "Your deposit of taka {$this->amount} was successful."
+        ];
+    }
+}
